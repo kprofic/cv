d975dbb089485208616610246ab5dcb42d33e231
Krzysztof Profic <kprofic@gmail.com>
2020-04-23 12:32:19 +0200

    Construct profile screen based on profile model

--- cv/Scenes/Profile/ProfileViewController.swift
+++ cv/Scenes/Profile/ProfileViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-
+    @IBOutlet weak var fullnameLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+    @IBOutlet weak var linkedInButton: UIButton!
+    
+    var viewModel: Profile? {
+        didSet {
+            guard let vm = viewModel else { return }
+            
+            fullnameLabel.text = vm.firstName + " " + vm.lastName
+            subtitleLabel.text = vm.positionName + " with \(vm.experience) years of experience"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        viewModel = Profile(firstName: "Krzysztof",
+                            lastName: "Profic",
+                            positionName: "Senior iOS Developer",
+                            experience: 9,
+                            linkedInUrl: URL(string: "https://www.linkedin.com/in/krzysztof-profic-b73b4421/")!)
     }
-
-
 }
 
