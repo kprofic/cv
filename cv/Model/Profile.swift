d5c132c26c52b3098d79fa66152e172e21f37bc6
Krzysztof Profic <kprofic@gmail.com>
2020-04-23 12:16:53 +0200

    Add Profile entity

---
+++ cv/Model/Profile.swift
@@ -0,0 +1,17 @@
+//
+//  Profile.swift
+//  cv
+//
+//  Created by Krzysiek on 23/04/2020.
+//  Copyright © 2020 kprofic. All rights reserved.
+//
+
+import Foundation
+
+struct Profile {
+    let firstName: String
+    let lastName: String
+    let positionName: String
+    let experience: UInt
+    let linkedInUrl: URL
+}
